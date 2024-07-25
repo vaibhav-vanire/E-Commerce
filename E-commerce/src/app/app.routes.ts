849937773fb402bf4d52a2d6e79a8b5381a0847d
Kartiki Vanire <vaibhavvanire11@gmail.com>
2024-07-25 20:30:58 +0530

    made changes in app.routes.ts

--- E-commerce/src/app/app.routes.ts
+++ E-commerce/src/app/app.routes.ts
@@ -8,6 +8,9 @@ import { UserCrudComponent } from './admin/user-crud/user-crud.component';
 import { ProductComponent } from './product/product.component';
 import { SigninSignupComponent } from './customer/signin-signup/signin-signup.component';
 import { SellerDashboardComponent } from './customer/seller/seller-dashboard/seller-dashboard.component';
+import { BuyerDashboardComponent } from './customer/buyer/buyer-dashboard/buyer-dashboard.component';
+import { CheckoutComponent } from './customer/buyer/checkout/checkout.component';
+import { PageNotFoundComponent } from './shared/layout/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "/", pathMatch: "full" },
@@ -37,5 +40,10 @@ export const routes: Routes = [
     {path:'',children:[
         {path:"seller-dashboard",component:SellerDashboardComponent},
         {path:"seller/product",component:ProductComponent}
-    ]}
+    ]},
+    {path:'',children:[
+        {path:"buyer-dashboard",component:BuyerDashboardComponent},
+        {path:"checkout",component:CheckoutComponent}
+    ]},
+    {path:"**",component:PageNotFoundComponent}
 ];
